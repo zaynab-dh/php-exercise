ddf7c7438a886cd6bb07629b4a7efb415f24bc8f
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-23 22:05:12 +0300

    step 5

--- basic-01/Step-5.php
+++ basic-01/Step-5.php
@@ -2,20 +2,17 @@
 $course_name = 'Rust, the language of no bugs';
 $enrolled_students = 20;
 $price = 29.99;
-$on_discount= false;
-if ($price <= 29.99)
+$on_discount= FALSE;
+if ($price < 29.99)
 {
+    $on_discount= TRUE; 
+}
     echo "Course title: $course_name <br />";
     echo "Enrolled Students: $enrolled_students  <br />";
-    echo "Course price:"(ceil($price))  " $ USD <br />";
-    echo "Course on discount: No";
-}
-else
-{
-    echo "Course title: $course_name <br />";
-    echo "Enrolled Students: $enrolled_students  <br />";
-    echo "Course price:"(ceil($price))  " $ USD <br />";
-    echo "Course on discount: Yes";
-}
+    echo "Course price:";
+    echo (round($price));
+    echo " $ USD <br />";
+    if ($on_discount) {echo "Course on discount: Yes";}
+    else{echo "Course on discount: No";}
 
 ?>
