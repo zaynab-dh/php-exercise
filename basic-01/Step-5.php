33173438117614f2ccd7f1909e4f11e73791b003
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-23 20:26:26 +0300

    step 6

--- basic-01/Step-5.php
+++ basic-01/Step-5.php
@@ -1,18 +1,20 @@
 <?php
 $course_name = 'Rust, the language of no bugs';
 $enrolled_students = 20;
-$price = 29,99 '$ USD';
-if ($price)
-{echo "Course title: $course_name <br />";
-echo "Enrolled Students: $enrolled_students  <br />";
-echo "Course price: ceil($price)   <br />";
-echo "Course on discount: No";}
-else 
-{
-    echo "Course title: $course_name <br />";
-    echo "Enrolled Students: $enrolled_students  <br />";
-    echo "Course price: (ceil($price))  <br />";
-    echo "Course on discount: Yes";
-}
+$price = 29,99;
+// $on_discount= false; 
+// {
+//     echo "Course title: $course_name <br />";
+//     echo "Enrolled Students: $enrolled_students  <br />";
+//     echo "Course price:"(ceil($price))  " $ USD <br />";
+//     echo "Course on discount: No";
+// }
+// if ($price < 29.99)
+// {
+//     echo "Course title: $course_name <br />";
+//     echo "Enrolled Students: $enrolled_students  <br />";
+//     echo "Course price:"(ceil($price))  " $ USD <br />";
+//     echo "Course on discount: Yes";
+// }
 
 ?>
---
+++ basic-01/Step-6.php
@@ -0,0 +1,13 @@
+<?php
+function calculateArea
+(
+$height,
+$width
+)
+{
+echo "Area is : ".$height*$width;
+}
+calculateArea(
+5,3
+)
+?>
