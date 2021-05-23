f1182b21ab43f54a9d23011fe8d3dfda4157af75
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-23 13:32:00 +0300

    step 3

---
+++ basic-01/Step-3.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Zaynab';
+$email = 'zaynab.dhaybi@gmail.com';
+$height = 50;
+$width = 10;
+$area = $height * $width;
+echo "My name is '$name' and my email is '$email'.";
+echo $area;
+?>
