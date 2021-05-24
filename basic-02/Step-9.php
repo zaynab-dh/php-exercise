f2dc6f2356ab3776d0ca06e338ec288c89694886
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-24 11:29:18 +0300

    basic-2 step-9

---
+++ basic-02/Step-9.php
@@ -0,0 +1,15 @@
+<?php
+function my_sqrt($n)
+{
+  $x = $n;
+  $y = 1;
+  while($x > $y)
+  {
+    $x = ($x + $y)/2;
+    $y = $n/$x;
+  }
+  return $x;
+}
+print_r(my_sqrt(16)."\n");
+print_r(my_sqrt(14)."\n");
+?>
