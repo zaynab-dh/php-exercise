3a9aa37d2818ccf7a66735c9c7010c9f6db2a943
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-24 12:34:07 +0300

    basic-3 step1 ex1

---
+++ basic-03/Step1-ex1.php
@@ -0,0 +1,26 @@
+<?php
+$i=1; 
+while ($i<9){ 
+    $x=0;
+    while ($x<$i){
+        echo"*";
+        $x++;
+    }
+    echo "<br/>";
+    $i++;
+} 
+?>
+
+
+
+
+<!-- $row=1;
+while ($row<9){   
+    $star=0;
+    while($star<$row){
+        echo "*";
+            $row++;
+    }
+    echo "<br>";
+    $star++;
+} -->
