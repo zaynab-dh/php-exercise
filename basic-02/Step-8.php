a623332b48489f45e7ab614fb7b9c854df5355f1
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-24 11:23:58 +0300

    basic-2 step-8

---
+++ basic-02/Step-8.php
@@ -0,0 +1,16 @@
+<?php
+function is_Power_of_two($n)
+{
+   if(($n & ($n - 1)) == 0)
+    {
+		return "$n is power of 2";
+    }
+   else
+    {
+		return "$n is not power of 2";
+    }
+}
+print_r(is_Power_of_two(4)."\n");
+print_r(is_Power_of_two(36)."\n");
+print_r(is_Power_of_two(16)."\n");
+?>
