e7ed3bb107c8759a55e5cec83c0b59eadf05db2b
zaynab-dh <zaynab.dhaybi@gmail.com>
2021-05-27 09:04:18 +0300

    part 2

--- exercise-01/Part-2/calculator.php
+++ exercise-01/Part-2/calculator.php
@@ -2,14 +2,14 @@
 <head>
     <meta charset="UTF-8">
 
-    <link rel="stylesheet" type="text/css" href="csss.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 
     <title></title>
 </head>
 <body>
     <table > 
 <tr> 
-<td colspan="2"></td> 
+<td colspan="1"></td> 
 <td>monthly</td>  
 
   <td>yearly </td> 
@@ -19,21 +19,23 @@
    <tr> 
    <td>Total salary</td> 
    <td><?php echo $_POST["Salary"]?> </td>
-   <td><?php echo $_POST["Salary"]?> </td>
+   <td><?php echo $_POST["Salary"] * 12 ?> </td>
 
    </tr>
    <tr> 
 
    <td>Tax amount</td> 
-
+   <td><?php echo $_POST["taxfree"]?> </td>
+   <td><?php echo $_POST["taxfree"] * 12 ?> </td>
  </tr>
  <tr> 
 <td>Social security fee</td> 
 </tr>
 <tr> 
    <td>Salary after tax  </td> 
-
-   </tr>
+   <td><?php echo $_POST["Salary"] - $_POST["taxfree"]?> </td>
+   <td><?php echo ($_POST["Salary"] - $_POST["taxfree"]) * 12 ?> </td>
+</tr>
 
 
     </table>
@@ -41,43 +43,43 @@
     
     <?php
 
-// if (isset($_POST['Calculate']))
-// {
-//     $salary = $_POST["Salary"];
-//     $taxfree = $_POST["taxfree"];
-//     $radioVal = $_POST["radio"];
+if (isset($_POST['Calculate']))
+{
+    $salary = $_POST["Salary"];
+    $taxfree = $_POST["taxfree"];
+    $radioVal = $_POST["radio"];
 
-//     if($radioVal == "Yearly")
-//     {
-//         $salary=$_POST["Salary"]*12;
-//         $taxfree = $_POST["taxfree"]*12;
-//     }
-//     else if ($radioVal == "monthly")
-//     {
-//     $salary=$_POST["Salary"];
-//     $taxfree = $_POST["taxfree"];
-//     }
-// }
-//     class MyCalculator {
+    if($radioVal == "Yearly")
+    {
+        $salary=$_POST["Salary"]*12;
+        $taxfree = $_POST["taxfree"]*12;
+    }
+    else if ($radioVal == "monthly")
+    {
+    $salary=$_POST["Salary"];
+    $taxfree = $_POST["taxfree"];
+    }
+}
+    class MyCalculator {
         
-//         public function __construct($salary, $taxfree,$radioVal)
-//         {
-//             $this->salary= $salary;
-//             $this->taxfree = $taxfree;
-//         }
-//         public function monthtotalsalary() {
-//             return $this->salary;
-//         }
-//         public function tax() {
-//             return $this->taxfree;
-//         }
-//         public function salaryaftertax() {
-//             return $this->salary - ($this->taxfree * $this->salary /100 );
-//         }
-//         public function divide() {
-//             return $this->value1 / $this->value2;
-//         }
-//     }
+        public function __construct($salary, $taxfree,$radioVal)
+        {
+            $this->salary= $salary;
+            $this->taxfree = $taxfree;
+        }
+        public function monthtotalsalary() {
+            return $this->salary;
+        }
+        public function tax() {
+            return $this->taxfree;
+        }
+        public function salaryaftertax() {
+            return $this->salary - ($this->taxfree * $this->salary /100 );
+        }
+        public function divide() {
+            return $this->value1 / $this->value2;
+        }
+    }
 
 ?>
 
